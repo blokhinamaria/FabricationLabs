1c92d7115db6469cc3943c4993b2754f706d1f28
blokhinamaria <blokhinamariyayu@gmail.com>
2025-10-19 15:36:04 -0400

    Set api routes and CORS methods with database connection

--- api/appointments.js
+++ api/appointments.js
@@ -2,6 +2,10 @@ import { handleGet, handlePut, handleDelete } from "../handlers/routeHandlers"
 import { sendResponse } from "../utils/sendResponse"
 
 export default async function handler(req, res) {
+    //CORS headers
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
             if (req.method === 'GET') {
                 return await handleGet(req, res)
