dbedd94d0061e7b3b20d127a7321364ef85bac67
Maria B <blokhinamariyayu@gmail.com>
2026-01-20 01:56:27 -0500

    Updated regex in checkDemoUsers.js

--- server/utils/checkDemoUsers.js
+++ server/utils/checkDemoUsers.js
@@ -1,4 +1,4 @@
 export function isDemoUser(email) {
-    const demoRegex = /^demo-(student|faculty|admin)@fabricationlabs\.com$/;
+    const demoRegex = /^demo-(student|faculty|admin)@fablabs\.com$/;
     return demoRegex.test(email);
 }
